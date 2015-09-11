8f80d3699ac268469533b559156fc8b764e2b832
vpramo <pramod.venkatesh@ril.com>
2015-09-11 19:49:03 +0530

    Fixed SSH banner issue

--- manifests/base.pp
+++ manifests/base.pp
@@ -37,7 +37,7 @@ class commonservice::base(
     options => {
       'PasswordAuthentication' => 'no',
       'PermitRootLogin'        => 'no',
-      'Banner'                 => '/etc/issue.net',
+      'Banner'                 => '/etc/issue',
       'Ciphers'                => 'aes128-ctr,aes192-ctr,aes256-ctr,arcfour256,arcfour128,aes128-gcm@openssh.com,aes256-gcm@openssh.com,chacha20-poly1305@openssh.com',
       'MACs'                   => 'hmac-sha1-etm@openssh.com,umac-64-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-ripemd160-etm@openssh.com,hmac-sha1-96-etm@openssh.com,hmac-sha1,umac-64@openssh.com,umac-128@openssh.com,hmac-sha2-256,hmac-sha2-512,hmac-ripemd160'
     },
