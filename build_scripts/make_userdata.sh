38ddbfab672d52d1ce42d2e4dd9272668c8aa301
vpramo <pramod.venkatesh@ril.com>
2015-09-11 09:15:54 +0530

    Changed rjil reference to commonservice reference

--- build_scripts/make_userdata.sh
+++ build_scripts/make_userdata.sh
@@ -47,14 +47,14 @@ done
 
 if [ -n "${puppet_modules_source_repo}" ]; then
   apt-get install -y git
-  git clone ${puppet_modules_source_repo} /tmp/rjil
+  git clone ${puppet_modules_source_repo} /tmp/commonservice
   if [ -n "${puppet_modules_source_branch}" ]; then
-    pushd /tmp/rjil
+    pushd /tmp/commonservice
     git checkout ${puppet_modules_source_branch}
     popd
   fi
   if [ -n "${pull_request_id}" ]; then
-    pushd /tmp/rjil
+    pushd /tmp/commonservice
     git fetch origin pull/${pull_request_id}/head:test_${pull_request_id}
     git config user.email "testuser@localhost.com"
     git config user.name "Test User"
@@ -63,21 +63,21 @@ if [ -n "${puppet_modules_source_repo}" ]; then
   fi
   time gem install librarian-puppet-simple --no-ri --no-rdoc;
   mkdir -p /etc/puppet/manifests.overrides
-  cp /tmp/rjil/site.pp /etc/puppet/manifests.overrides/
+  cp /tmp/commonservice/site.pp /etc/puppet/manifests.overrides/
   mkdir -p /etc/puppet/hiera.overrides
-  sed  -i "s/  :datadir: \/etc\/puppet\/hiera\/data/  :datadir: \/etc\/puppet\/hiera.overrides\/data/" /tmp/rjil/hiera/hiera.yaml
-  cp /tmp/rjil/hiera/hiera.yaml /etc/puppet
-  cp -Rvf /tmp/rjil/hiera/data /etc/puppet/hiera.overrides
+  sed  -i "s/  :datadir: \/etc\/puppet\/hiera\/data/  :datadir: \/etc\/puppet\/hiera.overrides\/data/" /tmp/commonservice/hiera/hiera.yaml
+  cp /tmp/commonservice/hiera/hiera.yaml /etc/puppet
+  cp -Rvf /tmp/commonservice/hiera/data /etc/puppet/hiera.overrides
   mkdir -p /etc/puppet/modules.overrides/commonservice
-  cp -Rvf /tmp/rjil/* /etc/puppet/modules.overrides/commonservice/
+  cp -Rvf /tmp/commonservice/* /etc/puppet/modules.overrides/commonservice/
   if [ -n "${module_git_cache}" ]
   then
     cd /etc/puppet/modules.overrides
     wget -O cache.tar.gz "${module_git_cache}"
     tar xvzf cache.tar.gz
-    time librarian-puppet update --puppetfile=/tmp/rjil/Puppetfile --path=/etc/puppet/modules.overrides
+    time librarian-puppet update --puppetfile=/tmp/commonservice/Puppetfile --path=/etc/puppet/modules.overrides
   else
-    time librarian-puppet install --puppetfile=/tmp/rjil/Puppetfile --path=/etc/puppet/modules.overrides
+    time librarian-puppet install --puppetfile=/tmp/commonservice/Puppetfile --path=/etc/puppet/modules.overrides
   fi
   cat <<INISETTING | puppet apply --config_version='echo settings'
   ini_setting { basemodulepath: path => "/etc/puppet/puppet.conf", section => main, setting => basemodulepath, value => "/etc/puppet/modules.overrides:/etc/puppet/modules" }
